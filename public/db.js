625771201087d7c354619df231954fc0cf9940ec
alexdmacon <alexdmacon@gmail.com>
2021-12-16 19:33:16 -0600

    got db.js file written

--- public/db.js
+++ public/db.js
@@ -1,13 +1,16 @@
 // create variable for database
 let db;
 
-// creating a new request to create a database called "budget"
-const request = indexedDB.open('BudgetDB', 1);
+// creating a new request to create a database called "BudgetDB"
+const request = indexedDB.open("BudgetDB", 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
 
+
+  if (db.objectStoreNames.length === 0) {
   db.createObjectStore("BudgetStore", { autoIncrement: true });
+  }
 };
 
 request.onerror = function (event) {
@@ -15,37 +18,67 @@ request.onerror = function (event) {
 };
 
 request.onsuccess = function (event) {
-    console.log('It worked, db created with object store');
-    db = event.target.result;
-  
-    if (navigator.onLine) {
-      console.log('We are online, reading from db');
-      checkDB();
+  console.log("It worked, db created with object store");
+  db = event.target.result;
 
-      // not sure if we need this else?
-    } else {
-        saveToDB();
+  if (navigator.onLine) {
+    console.log("We are online, reading from db");
+    checkDB();
+  }
+};
+
+
+function checkDB() {
+  console.log("check db invoked");
+
+  // Open a transaction on your BudgetStore db
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // access your BudgetStore object
+  const store = transaction.objectStore("BudgetStore");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+
+          if (res.length !== 0) {
+
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            currentStore.clear();
+          }
+        });
     }
   };
+}
 
-// need to write checkDB function to check and fetch from database if we're online, and saveToDB function to save to database if we're offline 
+// this is called by the sendTransaction function in index.js. if sendTransaction doesn't work (because the user is offline) and errors, then saveRecord is invoked to add add the posted record to the offline database, aka the indexed db. Can be retrieved when we go back online.
+const saveRecord = (record) => {
+  console.log("Save record invoked");
 
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
 
+  const store = transaction.objectStore("BudgetStore");
 
-
-
-
-// Listen for app coming back online
-window.addEventListener('online', checkDB);
-
-
-
-
+  store.add(record);
+};
 
 
-
-
-
-
-
-
+// if app goes online, then call checkDB function to refresh from database.
+window.addEventListener("online", checkDB);

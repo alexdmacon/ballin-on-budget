ce925bfb918e5066dcf510e1dd7860ca0ebb41bc
alexdmacon <alexdmacon@gmail.com>
2021-12-18 15:23:06 -0600

    getting ready to deploy on heroku and mongo atlas

--- public/index.js
+++ public/index.js
@@ -137,7 +137,7 @@ function sendTransaction(isAdding) {
   .catch(err => {
     // fetch failed, so save in indexed db
     saveRecord(transaction);
-    console.log("sending to indexed DB")
+
     // clear form
     nameEl.value = "";
     amountEl.value = "";
